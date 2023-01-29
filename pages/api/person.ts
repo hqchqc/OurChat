5d236a5eb4afd3a2ae08315703dd7bef95f67c68
hqchqc <734986933@qq.com>
2023-01-29 21:41:08 +0800

    fix: start study

--- pages/api/person.ts
+++ pages/api/person.ts
@@ -1,4 +1,9 @@
 import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
 
 const prisma = new PrismaClient()
 
@@ -7,10 +12,14 @@ const prisma = new PrismaClient()
 //   console.log(allUsers)
 // }
 
-export default async function handler() {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
   const post = await prisma.post.update({
     where: { id: 1 },
     data: { published: true },
   })
   console.log(post)
+  res.status(200).json({ name: 'John Doe' })
 }
